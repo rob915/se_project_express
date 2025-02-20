c4aa75e609be391b9f9dbe9d6bf4afaf1bdce0e2
Robert Rodriguez <rob915@gmail.com>
2025-02-19 22:04:02 -0800

    fix: added next as fourth argument to error handler and returned next

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,10 +1,11 @@
-const errorHandler = (err, req, res) => {
+const errorHandler = (err, req, res, next) => {
   console.error(err);
   const { statusCode = 500, message } = err;
   res.status(statusCode).send({
     message:
       statusCode === 500 ? "An error has occured on the server" : message,
   });
+  return next();
 };
 
 module.exports = errorHandler;
